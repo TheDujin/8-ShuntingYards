df172e6bb6eb54c6e79625b7994c19e2b309f207
U-BSD\293359 <293359@SHS-10DD3C387.beaverton.k12.or.us>
2017-02-21 14:30:21 -0800

    Changed stuff

--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -7,24 +7,63 @@
 
 #include <iostream>
 #include "Node.h"
+#include <string.h>
+#include <stdio.h>
 using namespace std;
 void push(Node* newNode, Node* & bottom);
 Node* pop(Node* & bottom);
 Node* peek(Node* & bottom);
 int main() {
+	char* input = new char[101];
 	Node* head = NULL;
-	push(new Node('a'), head);
+//	push(new Node('a'), head);
+//
+//	push(new Node('b'), head);
+//	push(new Node('c'), head);
+//	Node* current = head;
+//	while (current->getNext() != NULL) {
+//		cout << current->getData() << endl;
+//		current = current->getNext();
+//	}
+//	cout << current->getData() << endl;
 
-	push(new Node('b'), head);
-	push(new Node('c'), head);
-	Node* current = head;
-	while (current->getNext() != NULL) {
-		cout << current->getData() << endl;
-		current = current->getNext();
+	cout << "Please input the infix expression with spaces between each token." << endl << "Input: ";
+	cin.get(input, 100);
+	cin.ignore();
+	int counter = 0;
+	char* output = new char[101];
+	bool invalid = false;
+	for (int i = 0; i < strlen(input); i++) {
+		cout << input[i] << endl;
+		if (input[i] - '0' >= 0 && input[i] - '0' <= 9) {
+			output[counter] = input[i];
+			counter++;
+		}
+		else if (input[i] == '+' || input[i] == '-') {
+			cout << "AS" << endl;
+		}
+		else if (input[i] == '*' || input[i] == '/') {
+			cout << "MD" << endl;
+		}
+		else if (input[i] == '^') {
+			cout << "E" << endl;
+		}
+		else if (input[i] == '(') {
+			cout << "P" << endl;
+		}
+		else if (input[i] == ')') {
+			cout << "P" << endl;
+		}
+		else if (input[i] != '\0' && input[i] != '\r'){
+			cout << "Invalid input" << endl;
+			invalid = true;
+			i = strlen(input);
+		}
+
 	}
-	cout << current->getData() << endl;
-
-  return 0;
+	if (!invalid)
+		cout << output << endl;
+	return 0;
 }
 
 void push(Node* newNode, Node* & bottom) {
