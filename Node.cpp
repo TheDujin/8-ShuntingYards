a4564b07cd2ec95725b0c21c2c617fdec0ebd98e
U-BSD\293359 <293359@SHS-10DD3C387.beaverton.k12.or.us>
2017-02-23 13:49:18 -0800

    Added comments

--- Node.cpp
+++ Node.cpp
@@ -3,32 +3,32 @@
  *
  *  Created on: Feb 14, 2017
  *      Author: Kevin Jin
+ *  More detailed method descriptions are in the .h file.
  */
 
 #include <iostream>
 #include "Node.h"
 using namespace std;
 
-
+//Constructs a Node
 Node::Node(char newData) {
   data = newData;
   next = NULL;
 }
-
+//Return next
 Node* Node::getNext() {
   return next;
 }
-
+//Return data
 char Node::getData() {
   return data;
 }
-
+//Sets next Node
 void Node::setNext(Node* newNext) {
   next = newNext;
 }
-
+//Destructs the Node
 Node::~Node() {
-  //delete data;
   next = NULL;
 }
 
--- ShuntingYard.cpp
+++ ShuntingYard.cpp
@@ -3,6 +3,7 @@
  *
  *  Created on: Feb 14, 2017
  *      Author: Kevin Jin
+ *  This file uses the shunting yard algorithm to convert infix notation to postfix.
  */
 
 #include <iostream>
@@ -10,24 +11,33 @@
 #include <string.h>
 #include <stdio.h>
 using namespace std;
+
+//Define the functions used by a Stack data structure
 void push(Node* newNode, Node* & bottom);
 Node* pop(Node* & bottom);
 Node* peek(Node* & bottom);
+
+//Main method. Prompts for the infix expression, then converts it.
 int main() {
+	//Read in input and define the Stack's "head" (bottom of the stack) as NULL
 	char* input = new char[101];
 	Node* head = NULL;
 
 	cout << "Please input the infix expression with spaces between each token." << endl << "Input: ";
 	cin.get(input, 100);
 	cin.ignore();
+	//Creates output char* and uses a counter to keep track how far we've written into it.
 	int counter = 0;
 	char* output = new char[101];
+	//If the expression is invalid, bool invalid is set to true and program kills itself.
 	bool invalid = false;
 	for (int i = 0; i < strlen(input); i++) {
+		//For each char in input, if it's a number write it to output, else perform operations upon the operator stack.
 		if (input[i] - '0' >= 0 && input[i] - '0' <= 9) {
 			output[counter] = input[i];
 			counter++;
 		}
+		//If it's AS, pop out of the stack all operators with more precedence (including other AS) then push it onto the stack.
 		else if (input[i] == '+' || input[i] == '-') {
 			while (peek(head) != NULL && (peek(head)->getData() == '^' || peek(head)->getData() == '*' || peek(head)->getData() == '/'
 					|| peek(head)->getData() == '+' || peek(head)->getData() == '-')) {
@@ -36,6 +46,7 @@ int main() {
 						}
 						push(new Node(input[i]), head);
 		}
+		//If it's DM, ditto AS.
 		else if (input[i] == '*' || input[i] == '/') {
 			while (peek(head) != NULL && (peek(head)->getData() == '^' || peek(head)->getData() == '*' || peek(head)->getData() == '/')) {
 				output[counter] = pop(head)->getData();
@@ -43,12 +54,15 @@ int main() {
 			}
 			push(new Node(input[i]), head);
 		}
+		//If it's E, push it on (it will always have higher precedence than any other operators on the stack).
 		else if (input[i] == '^') {
 			push(new Node('^'), head);
 		}
+		//If it's a left paren, push it.
 		else if (input[i] == '(') {
 			push(new Node('('), head);
 		}
+		//If it's a right paren, pop everything between it and the corresponding left paren.
 		else if (input[i] == ')') {
 			while (peek(head)->getData() != '(') {
 				output[counter] = pop(head)->getData();
@@ -56,13 +70,16 @@ int main() {
 			}
 			pop(head);
 		}
+		//If it's anything else that isn't a natural part of a char* or isn't a space, we have an error.
 		else if (input[i] != '\0' && input[i] != '\r' && input[i] != ' '){
 			cout << "Invalid input" << endl;
+			//It's invalid, so program kills itself.
 			invalid = true;
 			i = strlen(input);
 		}
 
 	}
+	//If the expression is not invalid, pop the operator stack onto output then print output.
 	if (!invalid) {
 		while (peek(head) != NULL) {
 			output[counter] = pop(head)->getData();
@@ -72,14 +89,14 @@ int main() {
 	}
 	return 0;
 }
-
+//Pushes a node onto the top of the stack. It replaces bottom/head if it's NULL.
 void push(Node* newNode, Node* & bottom) {
 	if (peek(bottom) != NULL)
 		peek(bottom)->setNext(newNode);
 	else
 		bottom = newNode;
 }
-
+//Pop the topmost node, if there is one. If the topmost node is bottom/head, pop it then set it to NULL.
 Node* pop(Node* & bottom) {
 	Node* current = bottom;
 	Node* temp = NULL;
@@ -97,7 +114,7 @@ Node* pop(Node* & bottom) {
 	}
 	return temp;
 }
-
+//Returns the topmost node without popping it.
 Node* peek(Node* & bottom) {
 	Node* current = bottom;
 		if (current != NULL) {
